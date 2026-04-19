1f9293a80b291ed69cd6fd7129e44410967974b5
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-04-19 20:24:45 +0300

    2134 popcount/2 -> popcount

--- compress/src/main.rs
+++ compress/src/main.rs
@@ -78,7 +78,7 @@ fn compress_with_models(bytes: &[u8], models: &[bool; 128]) -> f64 {
                 .map(|(i, _v)| i)
             {
                 let c = stats[model].get_mut(apply_model_mask(prev_bytes, model as u8), next_byte);
-                let w = model.count_ones() as usize / 2; // + (c.c0 == 0 || c.c1 == 0) as usize * 2;
+                let w = model.count_ones() as usize; // + (c.c0 == 0 || c.c1 == 0) as usize * 2;
                 c0 += c.c0 << w;
                 c1 += c.c1 << w;
                 c.learn(bit);
