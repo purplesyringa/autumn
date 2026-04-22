7f92ec85c54d1af86dd28fbe7d61b9f0773a2a98
Alisa Sireneva <me@purplesyringa.moe>
2026-04-22 19:44:01 +0300

    Fix start address in explore

--- explore.sh
+++ explore.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-objdump -D --disassembler-color=color -m i386:x86-64 -M intel -b binary --adjust-vma=0x400000 --start-address=0x400078 interp-small | less -R
+objdump -D --disassembler-color=color -m i386:x86-64 -M intel -b binary --adjust-vma=0x400000 --start-address=0x400068 interp-small | less -R
